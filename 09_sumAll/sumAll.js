3a6761207e01a660c33870cfb7d2d196cdd90665
Evgeny Khatskov <45400445+YVM2L@users.noreply.github.com.>
2025-07-03 04:53:14 +0300

    console log

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -7,8 +7,14 @@ const sumAll = function(min, max) {
         max = temp;
         // меняем на верный порядок
     }
-
+    let sum = 0;
+    for (let i = min; i <= max; i++) { //считаем сумму
+        sum += i;
+    }
+    return sum
 };
+console.log(sumAll(1, 4));
+console.log(sumAll(-1, 10));
 
 // Do not edit below this line
 module.exports = sumAll;
