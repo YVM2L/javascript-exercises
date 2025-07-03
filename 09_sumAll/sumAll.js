a784c9559dd3035c7a9c679b7617bce2626ad946
Evgeny Khatskov <45400445+YVM2L@users.noreply.github.com.>
2025-07-03 04:43:59 +0300

    change the order of numbers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,12 @@
 const sumAll = function(min, max) {
-    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
-    if (min < 0 || max < 0) return "ERROR";
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR"; //проверяем целые числа
+    if (min < 0 || max < 0) return "ERROR"; // отрицательные числа запрещаются
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+        // меняем на верный порядок
+    }
 
 };
 
