24073369ee2f254822dcb3cb1cae57e2e0f79b93
Evgeny Khatskov <45400445+YVM2L@users.noreply.github.com.>
2025-07-03 03:39:01 +0300

    added result logics

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,7 +1,12 @@
 const repeatString = function(string, num) {
     if (num < 0) return "ERROR";
-
+    let result = "";
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+    return result
 };
+console.log(repeatString("Hello", 4));
 
 // Do not edit below this line
 module.exports = repeatString;
