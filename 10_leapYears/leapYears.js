a63d1b0f1448d6c8706368b3b96a398c1637d940
Evgeny Khatskov <45400445+YVM2L@users.noreply.github.com.>
2025-07-03 11:52:54 +0300

    if return year

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 === 0) return true;
+    if (year % 100 === 0) return false;
+    if (year % 4 === 0) return true;
+    return false;
 };
-
+console.log(2024, leapYears(2024));
+console.log(2025, leapYears(2025));
 // Do not edit below this line
 module.exports = leapYears;
