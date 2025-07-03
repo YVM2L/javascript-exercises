8b8e3fd4bcc9c6670d3a5722bd4affd6627855e8
Evgeny Khatskov <45400445+YVM2L@users.noreply.github.com.>
2025-07-03 04:40:15 +0300

    check for integers; negative numbers are not allowed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,6 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
 
 };
 
